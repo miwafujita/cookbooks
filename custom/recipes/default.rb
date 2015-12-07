1bd92d2043e63079ebcaeb8308c9849718600849
藤田 美和 <miwa@touda-no-MacBook-Air.local>
2015-12-07 18:46:18 +0900

    ディレクトリのシンボリックリンク作成

--- custom/recipes/default.rb
+++ custom/recipes/default.rb
@@ -1,7 +1,8 @@
-#include_recipe 'deploy'
-Chef::Log.debug("fjcs start!!!!!!!!!")
 node[:deploy].each do |application, deploy|
   link "#{deploy[:deploy_to]}/current/.env.php" do
     to "#{deploy[:deploy_to]}/shared/config/.env.php"
+  end
+  link "#{deploy[:deploy_to]}/current/app/storage" do
+    to "#{deploy[:deploy_to]}/shared/storage"
   end
 end
