f13f5a61555a406f717a50d41de2402d0cb62f6c
藤田 美和 <miwa@touda-no-MacBook-Air.local>
2015-12-07 18:14:15 +0900

    aaa

--- custom/recipes/default.rb
+++ custom/recipes/default.rb
@@ -1,10 +1,7 @@
-file "/tmp/hello.txt" do
-    action :create
-end
 #include_recipe 'deploy'
-#Chef::Log.debug("fjcs start!!!!!!!!!")
-#node[:deploy].each do |application, deploy|
-#  link "#{deploy[:deploy_to]}/current/config/.env.php" do
-#    to "#{deploy[:deploy_to]}/shared/config/.env.php"
-#  end
-#end
+Chef::Log.debug("fjcs start!!!!!!!!!")
+node[:deploy].each do |application, deploy|
+  link "#{deploy[:deploy_to]}/current/config/.env.php" do
+    to "#{deploy[:deploy_to]}/shared/config/.env.php"
+  end
+end
