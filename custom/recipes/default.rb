c8beed3f6e9c02f0757ce8aecb5b078e19a3a7d4
藤田 美和 <miwa@touda-no-MacBook-Air.local>
2015-12-07 18:31:55 +0900

    テスト

--- custom/recipes/default.rb
+++ custom/recipes/default.rb
@@ -1,7 +1,7 @@
-include_recipe 'deploy'
+#include_recipe 'deploy'
 Chef::Log.debug("fjcs start!!!!!!!!!")
 node[:deploy].each do |application, deploy|
-  link "#{deploy[:deploy_to]}/current/config/.env.php" do
+  link "#{deploy[:deploy_to]}/current/.env.php" do
     to "#{deploy[:deploy_to]}/shared/config/.env.php"
   end
 end
