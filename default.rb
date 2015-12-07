e08914877dba7d80c7ba35982f0f2a45d416c615
藤田 美和 <miwa@touda-no-MacBook-Air.local>
2015-12-07 17:49:49 +0900

    でバッグ

--- default.rb
+++ default.rb
@@ -1,3 +1,5 @@
+include_recipe 'deploy'
+Chef::Log.debug("fjcs start!!!!!!!!!")
 node[:deploy].each do |application, deploy|
   link "#{deploy[:deploy_to]}/current/config/.env.php" do
     to "#{deploy[:deploy_to]}/shared/config/.env.php"
